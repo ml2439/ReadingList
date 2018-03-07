512167d28de4bbb36ad2ee3e03ae178521aa425b
Andrew Bennet <AndrewBennet@users.noreply.github.com>
2018-03-07 08:43:40 +0000

    Bug fix - author sort and display not calculated for new Google-added books

---
+++ ReadingList/Models/Migrations/BookMapping_10_11.swift
@@ -0,0 +1,31 @@
+import Foundation
+import CoreData
+
+class BookMapping_10_11: NSEntityMigrationPolicy {
+    // This mapping exists to recalculate any author display / sort values which were missed out in v1.7.3
+    
+    @objc func authorSort(forAuthors: NSOrderedSet) -> String {
+        return forAuthors.map{
+            let author = ($0 as! NSManagedObject)
+            let lastName = sortable((author.value(forKey: "lastName") as! String))
+            let firstNames = sortable(author.value(forKey: "firstNames") as? String)
+            return [lastName, firstNames].flatMap{$0}.joined(separator: ".")
+            }.joined(separator: "..")
+    }
+    
+    @objc func authorDisplay(forAuthors: NSOrderedSet) -> String {
+        return forAuthors.map{
+            let author = ($0 as! NSManagedObject)
+            if let firstName = author.value(forKey: "firstNames") {
+                return "\(firstName) \(author.value(forKey: "lastName")!)"
+            }
+            return author.value(forKey: "lastName") as! String
+            }.joined(separator: ", ")
+    }
+    
+    func sortable(_ str: String?) -> String? {
+        guard let str = str else { return nil }
+        return str.folding(options: [.diacriticInsensitive, .caseInsensitive], locale: Locale.current)
+    }
+}
+
