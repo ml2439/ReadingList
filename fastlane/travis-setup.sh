3eb4fab1f71afa5963ff02118ba7d1c4e7be3589
Andrew Bennet <me@andrewbennet.com>
2018-03-18 13:14:58 +0000

    Testing CI beta deployment

---
+++ fastlane/travis-setup.sh
@@ -0,0 +1,5 @@
+# Add git certs HTTPS password to keychain
+fastlane actions setup_travis
+git config --global credential.helper osxkeychain
+git clone https://andrew_bennet:$GIT_CERTS_PASSWORD@bitbucket.org/andrew_bennet/certificates.git
+rm -rf certificates
