d8026d9c0a052988801691fb9fabec574dcf91bc
Andrew Bennet <me@andrewbennet.com>
2018-03-18 17:31:36 +0000

    Add cocoapods to fastlane

--- fastlane/travis-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Add git certs HTTPS password to keychain by cloning the certs repo
-git config --global credential.helper osxkeychain
-git clone https://andrew_bennet:$GIT_CERTS_PASSWORD@bitbucket.org/andrew_bennet/certificates.git
-rm -rf certificates
