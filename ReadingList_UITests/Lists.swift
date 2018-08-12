21b7cd37216cd7d08cd4fde5878a50562fd9d0e3
Andrew Bennet <me@andrewbennet.com>
2018-08-12 21:18:18 +0100

    Fix tests

--- ReadingList_UITests/Lists.swift
+++ ReadingList_UITests/Lists.swift
@@ -37,10 +37,10 @@ class Lists: XCTestCase {
 
         let modalTable = app.tables.element(boundBy: 0)
         XCTAssertEqual(modalTable.cells.count, 2, "Should be 2 table rows in modal list selector")
-        XCTAssertEqual(false, modalTable.cells.element(boundBy: 0).isEnabled, "Add to List A should not be hittable")
-        XCTAssertEqual(true, modalTable.cells.element(boundBy: 1).isEnabled, "Add to List A should not be hittable")
+        XCTAssertEqual(true, modalTable.cells.element(boundBy: 0).isEnabled, "Add New List should be hittable")
+        XCTAssertEqual(false, modalTable.cells.element(boundBy: 1).isEnabled, "Add to List A should not be hittable")
 
-        modalTable.cells.element(boundBy: 1).tap()
+        modalTable.cells.element(boundBy: 0).tap()
         XCTAssertEqual(app.alerts.count, 1, "Alert should appear when adding new list")
         let addList2Alert = app.alerts.firstMatch
         let enterList2NameTextField = addNewListAlert.collectionViews.textFields.firstMatch
