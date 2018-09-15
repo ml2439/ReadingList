ffd13910798c884d9eeaca69ed446ac65f7cd8b3
Andrew Bennet <me@andrewbennet.com>
2018-09-15 14:12:23 +0100

    Update device identifier table

--- ReadingList_Foundation/Extensions/UIKitExtensions.swift
+++ ReadingList_Foundation/Extensions/UIKitExtensions.swift
@@ -394,10 +394,7 @@ public extension UIDevice {
     var modelName: String {
         let identifier = modelIdentifier
         switch identifier {
-        case "iPod5,1":                                 return "iPod Touch 5"
         case "iPod7,1":                                 return "iPod Touch 6"
-        case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
-        case "iPhone4,1":                               return "iPhone 4s"
         case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
         case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
         case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
@@ -411,13 +408,14 @@ public extension UIDevice {
         case "iPhone10,1", "iPhone10,4":                return "iPhone 8"
         case "iPhone10,2", "iPhone10,5":                return "iPhone 8 Plus"
         case "iPhone10,3", "iPhone10,6":                return "iPhone X"
-        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
-        case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
+        case "iPhone11,2":                              return "iPhone XS"
+        case "iPhone11,4", "iPhone 11,6":               return "iPhone XS Max"
+        case "iPhone11,8":                              return "iPhone XR"
         case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
         case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
         case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
         case "iPad6,11", "iPad6,12":                    return "iPad 5"
-        case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
+        case "iPad7,5", "iPad7,6":                      return "iPad 6"
         case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
         case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
         case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
@@ -425,10 +423,6 @@ public extension UIDevice {
         case "iPad6,7", "iPad6,8":                      return "iPad Pro 12.9 Inch"
         case "iPad7,1", "iPad7,2":                      return "iPad Pro 12.9 Inch (2nd Generation)"
         case "iPad7,3", "iPad7,4":                      return "iPad Pro 10.5 Inch"
-        case "AppleTV5,3":                              return "Apple TV"
-        case "AppleTV6,2":                              return "Apple TV 4K"
-        case "AudioAccessory1,1":                       return "HomePod"
-        case "i386", "x86_64":                          return "Simulator"
         default:                                        return identifier
         }
     }
