a1885fd55446f59c61fa2a1cb3e72d9e3fdfe775
Andrew Bennet <me@andrewbennet.com>
2019-01-17 23:42:25 +0000

    Add fetch request response mocks

--- ReadingList_UITests/MockServer.swift
+++ ReadingList_UITests/MockServer.swift
@@ -4,11 +4,17 @@ import Swifter
 class MockServer {
     let server = HttpServer()
     private let searchedIsbns = ["9781111111111", "9780547345666", "9781781100264"]
-    
+    private let fetchedGoogleBookIds = ["gCtazG4ZXlQC"]
+
     init() {
         for isbn in searchedIsbns {
             server.get[GoogleBooksRequest.searchIsbn(isbn).relativePath] = { _ in
                 HttpResponse.ok(.json(self.jsonFromFile(withName: "Search_\(isbn)", ofType: "json") as AnyObject))
+            }
+        }
+        for googleBookId in fetchedGoogleBookIds {
+            server.get[GoogleBooksRequest.fetch(googleBookId).relativePath] = { _ in
+                HttpResponse.ok(.json(self.jsonFromFile(withName: "Fetch_\(googleBookId)", ofType: "json") as AnyObject))
             }
         }
     }
