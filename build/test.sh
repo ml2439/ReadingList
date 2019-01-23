bbc4bd03e0cd932a0baf039ee45d4dedddb0950d
Andrew Bennet <AndrewBennet@users.noreply.github.com>
2019-01-23 12:55:56 +0000

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 set -e
 
-xcodebuild \
+env NSUnbufferedIO=YES \
+ xcodebuild \
   -workspace ReadingList.xcworkspace \
   -scheme ReadingList \
-  -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' \
-  -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.4' \
-  -destination 'platform=iOS Simulator,name=iPhone XS,OS=12.1' \
-  -destination 'platform=iOS Simulator,name=iPad Pro (10.5-inch),OS=11.4' \
-  test | xcbeautify
+  -destination 'platform=iOS Simulator,id=839D5DB2-32BD-4484-AC84-A75180CF8FB4' \
+  -destination 'platform=iOS Simulator,id=D6E41B94-1BD3-45F8-B478-530FDE38E6B0' \
+  -destination 'platform=iOS Simulator,id=B40F7090-26EE-4D2E-A7A2-C13E10EDA669' \
+  -destination 'platform=iOS Simulator,id=C339A636-603E-425D-82B2-6FD8A064D2E6' \
+  build test | mint run thii/xcbeautify
